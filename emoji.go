cbdce5051009f614c4bc29ff924e97362ca8bc08
niwaniwa <niwaniwa@users.noreply.github.com>
2023-07-21 08:45:47 +0900

    remove: emojiIo
    emojiに統合

--- emoji.go
+++ emoji.go
@@ -6,12 +6,22 @@ import (
 	"github.com/yitsushi/go-misskey/core"
 	"github.com/yitsushi/go-misskey/models"
 	"github.com/yitsushi/go-misskey/services/notes"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 var (
 	emojiProcessList []Emoji
+	validExtensions  = map[string]bool{
+		".png":  true,
+		".jpg":  true,
+		".jpeg": true,
+		".gif":  true,
+	}
 )
 
 type Emoji struct {
@@ -131,4 +141,47 @@ func remove(val Emoji) {
 		}
 	}
 	emojiProcessList = newSlice
+}
+
+func emojiDownload(url string, filePath string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	dirPath := filepath.Dir(filePath)
+	// ディレクトリが存在するかチェック
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		// ディレクトリが存在しない場合、作成する
+		os.MkdirAll(dirPath, os.ModePerm)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deleteEmoji(filePath string) {
+	err := os.Remove(filePath)
+	if err != nil {
+		log.Printf("[ERROR] file not found %s\n", filePath)
+	}
+}
+
+func isValidEmojiFile(fileName string) bool {
+	fileExtension := filepath.Ext(fileName)
+	_, exists := validExtensions[fileExtension]
+	return exists
 }
