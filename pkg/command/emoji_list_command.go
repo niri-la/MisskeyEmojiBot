361083eb927f02dd02380d83bb19e00dadaeb91c
niwaniwa <konya.kokekokko@gmail.com>
2025-08-31 05:28:24 +0900

    add: emoji datail system

--- pkg/command/emoji_list_command.go
+++ pkg/command/emoji_list_command.go
@@ -29,6 +29,12 @@ func (c *emojiListCommand) GetCommand() *discordgo.ApplicationCommand {
 		Options: []*discordgo.ApplicationCommandOption{
 			{
 				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "id",
+				Description: "特定の絵文字IDの詳細を表示",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
 				Name:        "filter",
 				Description: "表示する申請の状態",
 				Required:    false,
@@ -60,10 +66,23 @@ func (c *emojiListCommand) GetCommand() *discordgo.ApplicationCommand {
 }
 
 func (c *emojiListCommand) Execute(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// フィルター引数を取得
+	var emojiID string
 	filter := "all"
-	if len(i.ApplicationCommandData().Options) > 0 {
-		filter = i.ApplicationCommandData().Options[0].StringValue()
+
+	// オプションを解析
+	for _, option := range i.ApplicationCommandData().Options {
+		switch option.Name {
+		case "id":
+			emojiID = option.StringValue()
+		case "filter":
+			filter = option.StringValue()
+		}
+	}
+
+	// IDが指定されている場合は詳細表示
+	if emojiID != "" {
+		c.showEmojiDetail(s, i, emojiID)
+		return
 	}
 
 	emojis := c.emojiRepository.GetEmojis()
@@ -172,6 +191,89 @@ func (c *emojiListCommand) Execute(s *discordgo.Session, i *discordgo.Interactio
 	}
 
 	// Discordのメッセージ長制限(2000文字)を考慮
+	contentStr := content.String()
+	if len(contentStr) > 1900 {
+		contentStr = contentStr[:1900] + "\n\n... (文字数制限により省略)"
+	}
+
+	_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags:   discordgo.MessageFlagsEphemeral,
+			Content: contentStr,
+		},
+	})
+}
+
+func (c *emojiListCommand) showEmojiDetail(s *discordgo.Session, i *discordgo.InteractionCreate, emojiID string) {
+	emoji, err := c.emojiRepository.GetEmoji(emojiID)
+	if err != nil || emoji == nil {
+		_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Flags:   discordgo.MessageFlagsEphemeral,
+				Content: fmt.Sprintf("❌ ID `%s` の絵文字が見つかりませんでした。", emojiID),
+			},
+		})
+		return
+	}
+
+	var content strings.Builder
+	content.WriteString(fmt.Sprintf("# ★絵文字詳細情報\n"))
+	content.WriteString(fmt.Sprintf("**ID:** `%s`\n", emoji.ID))
+	content.WriteString(fmt.Sprintf("**名前:** `%s`\n", emoji.Name))
+	content.WriteString(fmt.Sprintf("**申請者:** <@%s>\n", emoji.RequestUser))
+	content.WriteString(fmt.Sprintf("**申請日:** %s\n", emoji.StartAt.Format("2006年01月02日 15:04:05")))
+
+	if emoji.Category != "" {
+		content.WriteString(fmt.Sprintf("**カテゴリ:** %s\n", emoji.Category))
+	}
+	if emoji.Tag != "" {
+		content.WriteString(fmt.Sprintf("**タグ:** %s\n", emoji.Tag))
+	}
+	if emoji.License != "" {
+		content.WriteString(fmt.Sprintf("**ライセンス:** %s\n", emoji.License))
+	}
+	if emoji.Other != "" {
+		content.WriteString(fmt.Sprintf("**その他:** %s\n", emoji.Other))
+	}
+
+	content.WriteString(fmt.Sprintf("**NSFW:** %s\n", map[bool]string{true: "はい", false: "いいえ"}[emoji.IsSensitive]))
+	content.WriteString(fmt.Sprintf("**上書き:** %s\n", map[bool]string{true: "はい", false: "いいえ"}[emoji.IsOverwrite]))
+
+	// ステータス情報
+	content.WriteString("\n## 📊 ステータス情報\n")
+	if emoji.IsFinish {
+		if emoji.IsAccepted {
+			content.WriteString("**状態:** ✅ 承認済み\n")
+		} else {
+			content.WriteString("**状態:** ❌ 却下済み\n")
+		}
+	} else if emoji.IsRequested {
+		content.WriteString("**状態:** ⏳ 審査中\n")
+		content.WriteString(fmt.Sprintf("**承認数:** %d\n", emoji.ApproveCount))
+		content.WriteString(fmt.Sprintf("**却下数:** %d\n", emoji.DisapproveCount))
+	} else {
+		content.WriteString("**状態:** 🔄 申請前\n")
+	}
+
+	// テクニカル情報
+	content.WriteString("\n## 🔧 テクニカル情報\n")
+	if emoji.ChannelID != "" {
+		content.WriteString(fmt.Sprintf("**チャンネル:** <#%s>\n", emoji.ChannelID))
+	}
+	if emoji.UserThreadID != "" {
+		content.WriteString(fmt.Sprintf("**ユーザースレッド:** <#%s>\n", emoji.UserThreadID))
+	}
+	if emoji.ModerationMessageID != "" {
+		content.WriteString(fmt.Sprintf("**モデレーションメッセージID:** `%s`\n", emoji.ModerationMessageID))
+	}
+	if emoji.FilePath != "" {
+		content.WriteString(fmt.Sprintf("**ファイルパス:** `%s`\n", emoji.FilePath))
+	}
+	content.WriteString(fmt.Sprintf("**現在のステップ:** %d\n", emoji.NowStateIndex))
+	content.WriteString(fmt.Sprintf("**レスポンスフラグ:** %t\n", emoji.ResponseFlag))
+
 	contentStr := content.String()
 	if len(contentStr) > 1900 {
 		contentStr = contentStr[:1900] + "\n\n... (文字数制限により省略)"
